79c1c5c9f6b9561352ce3074cdf02d96905132ae
walkerfisher <101424637+walkerfisher@users.noreply.github.com>
2022-04-22 12:14:35 -0700

    ls functioning for Walker

--- bash.js
+++ bash.js
@@ -3,10 +3,11 @@ process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
+    const pwd = require('./pwd');
+    const ls = require('./ls');
     
-    if (cmd === 'pwd'){
-    console.log(process.cwd());
-    }
+    if (cmd === 'pwd') pwd();
+    if (cmd === 'ls') ls();
     
     process.stdout.write('\nprompt > ');
 })
